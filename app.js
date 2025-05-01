c6f337340e01f9c35b1e778ab910c02b24f3029d
urmila dubhase <DUBHASU@itsnet.bt.com>
2025-05-01 14:48:07 +0530

    fifth commit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
     import React from 'react';
     import ReactDOM from 'react-dom/client'; 
 
+    //Created the H1 Element by the react
+
     const heading = React.createElement("div", {id:"parent"},
       React.createElement("div", {id:"child"},
 
@@ -12,5 +14,42 @@
         ]
       )
     );
+
+    //Created the H1 Element by the JSX
+
+    const jsxHeaing = <h1 id="heading">Namste REact by JSX</h1>;
+    console.log(jsxHeaing);
+   
     const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading);
+    
+
+    //React-Functional Component- NEW both statements HeadingComponent & HeadingComponent2 are same as 
+    // HeadingComponent2 is the shortcut for one liner code
+
+    const HeadingComponent = () => {
+      return <h1>Namaste React by functional component </h1>;
+    
+    };
+
+    //component composition Example below also i can add the JS code inside the round braces by using curly braces
+    //example
+    const number = 1000; //we can use this JS no inside the below code as JSX is the mix of HTML and JS
+    //I can write any JS code inside the curly braces
+    const HeadingComponent3 = () =>{
+     return(
+      <div>
+        {jsxHeaing}
+         <h1>{number}</h1>
+         <h2>{console.log(HeadingComponent)}</h2>
+        <HeadingComponent />  
+        <HeadingComponent></HeadingComponent> //Bothe statements are same 
+         <h1>Namaste React by functional component without return "//component composition Example"</h1> 
+      </div>
+     );
+    };
+
+    const HeadingComponent2 = () => <h1>Namaste React by functional component without return </h1>;//one liner code
+   
+    root.render(<HeadingComponent3/>);
+
+
